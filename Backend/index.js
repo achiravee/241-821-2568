fac8327c1902972eb8cc8d21b1344571e055d51f
achiravee <achiravee.j@ku.th>
2026-02-28 11:43:27 +0700

    REST API

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const app = express();
 const mysql = require('mysql2/promise')
 const port = 8000; // ห้ามลืมบรรทัดนี้
+app.use(bodyParser.json());
 
 // ต้องมีบรรทัดนี้เพื่อให้ Server อ่าน JSON จาก Body ได้
 app.use(express.json());
@@ -28,14 +29,86 @@ app.get ('/users',async (req,res) => {
 
 //path = POST /test
 app.post('/users',async(req,res) => {
-    let user = req.body;
-    const results = await conn.query('INSERT INTO users SET ?',user);
-    res.json({
+    try{
+       let user = req.body;
+        const results = await conn.query('INSERT INTO users SET ?',user);
+        res.json({
         message: 'User created successfully',
         data:results[0]
-    });   
-})
+        }) 
+    }catch (error){
+        console.error('Error creating user:',error);
+        res.status(500).json({
+            message:'Error creating user',
+            error: error.message
+        });
+    }
+});
     
+app.get('/users/:id', async (req,res) => {
+    try{
+        let id = req.params.id
+        const results = await conn.query('SELECT * FROM users WHERE id = ?',id)
+        if (results[0].length == 0){
+            throw {statusCode: 404,message:'Uer not found'};
+        }
+        res.json(results[0][0]);
+    }
+    catch (error) {
+        console.error('Error fetching user:',error.message);
+        let statusCode = error.statusCode || 500;
+        res.status(statusCode).json({
+            message:'Error fetching user',
+            error:error.message
+        });
+    }
+})
+
+//PUT /user/:id สำหรับการแก้ไขข้อมูล user ที่มี id ตรงกับที่ส่งมา
+app.put('/users/:id', async (req,res) => {
+    try{
+        let id = req.params.id
+        let updatedUser = req.body;
+        const results = await conn.query('SELECT * FROM users WHERE id = ?',[updatedUser,id])
+        if (results[0].affectedRows == 0){
+            throw {statusCode: 404, message:'Uer not found'};
+        }
+        res.json({
+            message:'User updated successfully',
+            data: updatedUser
+        });
+    }
+    catch (error) {
+        console.error('Error fetching user:',error.message);
+        let statusCode = error.statusCode || 500;
+        res.status(statusCode).json({
+            message:'Error fetching user',
+            error:error.message
+        });
+    }
+})
+
+app.delete('/users/:id', async (req,res) => {
+    try{
+        let id = req.params.id
+        const results = await conn.query('DELETE FROM users WHERE id = ?',id)
+        if (results[0].affectedRows == 0){
+            throw {statusCode: 404,message:'Uer not found'};
+        }
+        res.json({
+            message: 'User deleted successfully'
+        });
+    }
+    catch (error) {
+        console.error('Error fetching user:',error.message);
+        let statusCode = error.statusCode || 500;
+        res.status(statusCode).json({
+            message:'Error fetching user',
+            error:error.message
+        });
+    }
+})
+
 //path = PUT /user/:id
 app.patch('/user/:id',(req,res) => {
     let id = req.params.id;
